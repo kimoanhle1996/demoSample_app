17007ee3d47a8ca5364fa8847c421d73896a30a2
Kim Oanh <kimoanhle1996@gmail.com>
2017-07-31 09:44:28 +0700

    modeling-users

--- Gemfile
+++ Gemfile
@@ -4,11 +4,9 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
-
-
 gem "bootstrap-sass", "3.3.7"
 gem "rails", "~> 5.1.2"
-
+gem "bcrypt",         "3.1.11"
 gem "sqlite3"
 gem "puma", "~> 3.7"
 gem "sass-rails", "~> 5.0"
@@ -33,5 +31,4 @@ group :development do
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
 end
-
 gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,15 @@
 Rails.application.routes.draw do
-<<<<<<< af3850debda1368bb57e6d1dfbac5b808da6204b
   get "users/new"
-=======
->>>>>>> Finish static pages
+
+  get 'users/new'
 
   root "static_pages#home"
 
   get "/help", to: "static_pages#help"
-<<<<<<< af3850debda1368bb57e6d1dfbac5b808da6204b
 
   get "/about", to: "static_pages#about"
 
   get "/contact", to: "static_pages#contact"
 
   get "/signup", to: "users#new"
-=======
-
-  get "/about", to: "static_pages#about"
-
-  get "/contact", to: "static_pages#contact"
-
->>>>>>> Finish static pages
 end
